0607d3522c835ffae60abf21f7d0277e4bdb8e80
Alvayang923 <90802085+Alvayang923@users.noreply.github.com>
2021-11-08 15:05:08 -0500

    Added working version

--- cipher_ry2403/tests/test_cipher_ry2403.py
+++ cipher_ry2403/tests/test_cipher_ry2403.py
@@ -1,1 +1,8 @@
-from cipher_ry2403 import cipher_ry2403
+from cipher_ry2403 import cipher_ry2403 
+import pytest 
+
+def test_cipher_single():
+    example = 'test'
+    expected = 'vguv'
+    actual = cipher_ry2403.cipher(example,2,True)
+    assert actual == expected,'not works for a single word'
